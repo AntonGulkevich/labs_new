cfe79e10a2b910591341287ceac7b2371cec21e4
Danil Nikonov <dan.nikonov@gmail.com>
2013-10-02 01:44:40 +0400

    added command line argument

--- socket_msg/client.cpp
+++ socket_msg/client.cpp
@@ -1,7 +1,7 @@
 #include "client.h"
 
 using namespace std;
-int main() {
+int main(int argc, char **argv) {
     int sock;
     int bufsize = 1024;
     char buffer[bufsize];
@@ -9,7 +9,10 @@ int main() {
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(55555);
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (argc == 1)
+        address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (argc == 2)
+        inet_aton(argv[1], &(address.sin_addr));
 
     sock = socket(AF_INET,SOCK_STREAM,0);
     if (sock <= 0) {
@@ -25,6 +28,8 @@ int main() {
     }    
     cout << "The connection was accepted with the server..." << endl;
 
+    int bytes_recieved;
+    int bytes_send;
     while (1) {
         cout << "Client: ";
         cin >> buffer;
@@ -32,9 +37,12 @@ int main() {
             send(sock, "Good bye", bufsize, 0);
             break;
         }
-        send(sock, buffer, bufsize, 0);
-        recv(sock, buffer, bufsize, 0);
-        cout << "Server: " << buffer << endl;
+        bytes_send = send(sock, buffer, bufsize, 0);
+        if (bytes_send == 0)
+            break;
+        bytes_recieved = recv(sock, buffer, bufsize, 0);
+        if (bytes_recieved > 0)
+            cout << "Server: " << buffer << endl;
     }
 
     close(sock);
--- socket_msg/server.cpp
+++ socket_msg/server.cpp
@@ -1,15 +1,19 @@
 #include "server.h"
 
 using namespace std;
-int main() {
+int main(int argc, char **argv) {
     int listener;
     int bufsize = 1024;
     char buffer[bufsize];
     
     struct sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     address.sin_port = htons(55555);
+    if (argc == 1)
+        address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (argc == 2)
+        inet_aton(argv[1], &(address.sin_addr));
+
 
     listener = socket(AF_INET,SOCK_STREAM,0);
     if (listener <= 0) {
@@ -32,16 +36,22 @@ int main() {
         cout << "Client was connected!" << endl;
     }
 
+    int bytes_recieved;
+    int bytes_send;
     while (1) {
-        recv(sock, buffer, bufsize, 0);
-        cout << "Client: " << buffer << endl;
+        bytes_recieved = recv(sock, buffer, bufsize, 0);
+        if (bytes_recieved > 0)
+            cout << "Client: " << buffer << endl;
         cout << "Server: ";
         cin >> buffer;
         if (!strcmp(buffer, "quit")) {
             send(sock, "Good bye", bufsize, 0);
             break;
         }
-        send(sock, buffer, bufsize, 0);
+        bytes_send = send(sock, buffer, bufsize, 0);
+
+        if (bytes_send == 0)
+            break;
     }
     close(sock);
     close(listener);
